da203b33b56e864810a4e0a044825867f6316f3c
M-jerez <jerez.m82@gmail.com>
2018-03-01 17:47:42 +0000

    language files update!

--- theme/lang/en_US.php
+++ theme/lang/en_US.php
@@ -6,63 +6,108 @@
 return array( 
 
 
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/functions.php : line 28 */
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/utils.php : line 124 */
 "wp-nuxt cant read the config file at <code>%s</code>"
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/utils.php : line 33 */
-"wp-nuxt config error: configuration for module '%s' missing from the config file!"
-=>
-""
-,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 15 */
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 18 */
 "Wp Nuxt Config"
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 20 */
-"Node.js"
-=>
-""
-,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 25 */
-"Node.js path"
-=>
-""
-,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 29 */
-"The path in the system to the node.js binary o executable file."
-=>
-""
-,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 40 */
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 24 */
 "Nuxt"
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 44 */
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 28 */
+"Node.js path"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 32 */
+"The path in the system to the node.js binary o executable file."
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 37 */
 "Nuxt root path"
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 49 */
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 43 */
 "The root directory of the Nuxt project, <code>nuxt generate</code> command will be executed from this directory."
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 50 */
-"This directory contains the file <code>nuxt.config.js</code>"
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 44 */
+"This directory also contains the file <code>nuxt.config.js</code>"
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 58 */
-"Enabled"
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 53 */
+"Automatic Static Site Generation"
 =>
 ""
 ,
-/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 65 */
-"Nuxt.js is an static site generator: for more info please visit their <a href='https://nuxtjs.org/' target='_blank'>documentation.</a>"
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 56 */
+"When Enabled, the <code>nuxt generate</code> command will be executed after a post is created, updated or deleted."
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 64 */
+"Configures Nuxt.js Static Site Generator: for more info about Nuxt.js please visit their <a href='https://nuxtjs.org/' target='_blank'>documentation.</a>"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 72 */
+"Rest"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 87 */
+"List All WP Menus on the Rest API"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 87 */
+"This Options Also Enables the WP Menus page on the Admin panel."
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 98 */
+"Disable the endpoint to List all WP users"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 98 */
+"(better security)"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 114 */
+"Cache REST API Endpoints"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 117 */
+"When Enabled, all REST API endpoints will be cached, the cache is completely cleared after a post is modified"
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 125 */
+"Adds some extra functionality to the Wordpress REST API, like new endpoints and caching."
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 126 */
+"Also disables the existing User Endpoint as a security measure."
+=>
+""
+,
+/* /Applications/AMPPS/www/mjerez/wp-nuxt/theme/admin/pages/admin_page.php : line 127 */
+"By default, WordPress allows to list all user accounts, which might be a security risk."
 =>
 ""
 ,
